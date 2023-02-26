9f7fef8873a9040964b6358ccf763e08bcc51dca
salvationlloydstanley-kemuel <skemuel008@gmail.com>
2023-02-27 00:42:07 +0100

    Add writeline indicating we are in command line

--- ProgitForCoders/ProgitForCoders/Program.cs
+++ ProgitForCoders/ProgitForCoders/Program.cs
@@ -5,6 +5,7 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("I just added this in Visual Studio");
+        Console.WriteLine("I just added this in the command line repo");
     }
 }
 
