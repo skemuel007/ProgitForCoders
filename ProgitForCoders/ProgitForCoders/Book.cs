92fa5c049adf977a8fbbb5414527e50ff77eb7c2
salvationlloydstanley-kemuel <skemuel008@gmail.com>
2023-02-27 01:20:08 +0100

    Add Properties to Book class in Book.cs file
    Properties title, authors as list and publication date was added to book class

---
+++ ProgitForCoders/ProgitForCoders/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ProgitForCoders
+{
+	public class Book
+	{
+		public Book()
+		{
+		}
+
+		public string Title { get; set; }
+		public List<string> Authors { get; set; }
+		public DateTime PublicationDate { get; set; }
+	}
+}
+
